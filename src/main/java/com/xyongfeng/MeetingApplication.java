6fd1dc21578ffba4a5de1036e39f0a883f7cbf0c
咏烽丶 <12917603+xyongfeng@user.noreply.gitee.com>
2023-09-24 08:49:21 +0800

    修复socket.io配置更新之后，无法生效

--- src/main/java/com/xyongfeng/MeetingApplication.java
+++ src/main/java/com/xyongfeng/MeetingApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class MeetingApplication implements CommandLineRunner {
+
     @Autowired
     private SocketIOServer socketIOServer;
 
--- src/main/java/com/xyongfeng/config/SocketIOConfig.java
+++ src/main/java/com/xyongfeng/config/SocketIOConfig.java
@@ -1,16 +1,40 @@
 package com.xyongfeng.config;
 
+import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import com.xyongfeng.pojo.config.SocketIOPro;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class SocketIOConfig {
-    @Autowired
-    private SocketIOPro socketIoPro;
+
+    @Value("${socketio.host}")
+    private String host;
+
+    @Value("${socketio.port}")
+    private Integer port;
+
+    @Value("${socketio.bossCount}")
+    private int bossCount;
+
+    @Value("${socketio.workCount}")
+    private int workCount;
+
+    @Value("${socketio.allowCustomRequests}")
+    private boolean allowCustomRequests;
+
+    @Value("${socketio.upgradeTimeout}")
+    private int upgradeTimeout;
+
+    @Value("${socketio.pingTimeout}")
+    private int pingTimeout;
+
+    @Value("${socketio.pingInterval}")
+    private int pingInterval;
 
     /**
      * SocketIO配置
@@ -18,10 +42,23 @@ class SocketIOConfig {
      */
     @Bean
     public SocketIOServer socketIoServer() {
-        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        config.setHostname("localhost");
-        config.setPort(9092);
+//        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+//        config.setHostname("localhost");
+//        config.setPort(9092);
 
+        SocketConfig socketConfig = new SocketConfig();
+        socketConfig.setTcpNoDelay(true);
+        socketConfig.setSoLinger(0);
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setSocketConfig(socketConfig);
+        config.setHostname(host);
+        config.setPort(port);
+        config.setBossThreads(bossCount);
+        config.setWorkerThreads(workCount);
+        config.setAllowCustomRequests(allowCustomRequests);
+        config.setUpgradeTimeout(upgradeTimeout);
+        config.setPingTimeout(pingTimeout);
+        config.setPingInterval(pingInterval);
         return new SocketIOServer(config);
     }
 
